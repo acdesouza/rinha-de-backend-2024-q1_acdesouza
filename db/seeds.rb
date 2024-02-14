90340b359d21177f937f8ec698251b6ce77374ae
Antonio Carlos da Graça Mota Durão de Souza <ac.desouza@gmail.com>
2024-02-13 22:04:11 -0300

    Creates Cliente model initial data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,17 @@
 # This file should ensure the existence of records required to run the application in every environment (production,
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+
+[
+  [1,   100000, 0],
+  [2,    80000, 0],
+  [3,  1000000, 0],
+  [4, 10000000, 0],
+  [5,   500000, 0]
+].each do |cliente_row|
+  Cliente.create(
+    id:            cliente_row[0],
+    limite:        cliente_row[1],
+    saldo_inicial: cliente_row[2],
+  )
+end
